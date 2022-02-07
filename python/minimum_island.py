667f641e354bac070f7f7fbe6b1d3696f45b53b6
Rohit Sharma <rohit.sharma1048@gmail.com>
2022-02-06 23:48:22 -0600

    add challenge smallest island

---
+++ python/minimum_island.py
@@ -0,0 +1,68 @@
+"""
+island count
+
+Write a function, island_count, that takes in a grid containing Ws and Ls.
+W represents water and L represents land. 
+The function should return the number of islands on the grid. 
+An island is a vertically or horizontally connected region of land.
+
+grid = [
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'W', 'W', 'L', 'W'],
+  ['W', 'W', 'L', 'L', 'W'],
+  ['L', 'W', 'W', 'L', 'L'],
+  ['L', 'L', 'W', 'W', 'W'],
+]
+"""
+
+def minimum_island(grid):
+
+    visited = set()
+    minimum = len(grid) * len(grid[0])
+
+    # let's loop through all the elements of the grid
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if traverse_grid(grid, r, c, visited) == True:
+              if minimum > len(visited): minimum = len(visited)
+              visited = set()
+    return minimum
+
+def traverse_grid(grid, r, c, visited):
+    # check if the r and c are in bounds
+    if not (0 <= r < len(grid)) or not (0 <= c < len(grid[0])):
+        return False
+
+    # if you reach water return
+    if grid[r][c] == 'W':
+        return False
+
+    # create a way to figure out the positions you have visited
+    visited_node = str(r) + ',' + str(c)
+
+    # check if already visited
+    if visited_node in visited:
+        return False
+
+    # otherwise add the current node to visited
+    visited.add(visited_node)
+
+    # Now we can traverse in the Land area up, down, left and right
+    traverse_grid(grid, r + 1, c, visited)
+    traverse_grid(grid, r - 1, c, visited)
+    traverse_grid(grid, r, c - 1, visited)
+    traverse_grid(grid, r, c + 1, visited)
+
+    return True
+
+grid = [
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'W', 'W', 'L', 'W'],
+  ['W', 'W', 'L', 'L', 'W'],
+  ['L', 'W', 'W', 'L', 'L'],
+  ['L', 'L', 'W', 'W', 'W'],
+]
+
+print(minimum_island(grid)) # -> 2
